7f86c3e79873eaf35f7eb714541ae0c4cb328c93
augustineezinwa <augustineezinwa@gmail.com>
2018-03-16 08:23:56 +0100

    Write businessValidation middle ware to validate against invalid inputs in the database

--- server/lib/middlewares/BusinessValidation.js
+++ server/lib/middlewares/BusinessValidation.js
@@ -1,5 +1,9 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _InputFieldsValidation = require('../helper/InputFieldsValidation');
@@ -11,7 +15,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var validateLocation = _InputFieldsValidation2.default.validateLocation,
-    validateCategory = _InputFieldsValidation2.default.validateCategory;
+    validateCategory = _InputFieldsValidation2.default.validateCategory,
+    validateBusinessTextFields = _InputFieldsValidation2.default.validateBusinessTextFields;
 
 /**
  * @class InputFieldsValidaton
@@ -20,45 +25,158 @@ var validateLocation = _InputFieldsValidation2.default.validateLocation,
  *
  */
 
-var UserValidation = function () {
-      function UserValidation() {
-            _classCallCheck(this, UserValidation);
-      }
+var BusinessValidation = function () {
+    function BusinessValidation() {
+        _classCallCheck(this, BusinessValidation);
+    }
 
-      _createClass(UserValidation, null, [{
-            key: 'validateBusiness',
+    _createClass(BusinessValidation, null, [{
+        key: 'validateBusiness',
 
 
-            /**
-               * @static
-               *
-               *@description -This method validates business Categories in WEConnect
-                *
-                * @param {object} req - The request payload sent to the router
-                * @param {object} res - The response payload sent back from the controller
-                * @param {object} next
-                * @returns {object} - status Message and logins user into WEConnect
-                *
-                * @memberOf UserController
-                *
-                */
-            value: function validateBusiness(req, res, next) {
+        /**
+           * @static
+           *
+           *@description -This method validates business Categories in WEConnect
+            *
+            * @param {object} req - The request payload sent to the router
+            * @param {object} res - The response payload sent back from the controller
+            * @param {object} next
+            * @returns {object} - status Message and logins user into WEConnect
+            *
+            * @memberOf UserController
+            *
+            */
+        value: function validateBusiness(req, res, next) {
 
-                  var business = {
+            var business = {
 
-                        businessName: req.body.businessName,
+                businessName: validateBusinessTextFields(req.body.businessName),
 
-                        businessAddress: req.body.businessAddress,
+                businessAddress: validateBusinessTextFields(req.body.businessAddress),
 
-                        businessDescription: req.body.businessDescription,
+                businessDescription: validateBusinessTextFields(req.body.businessDescription),
 
-                        location: validateLocation(req.body.location),
+                businessImage: req.body.businessImage,
 
-                        category: validateCategory(req.body.category)
+                location: validateLocation(req.body.location),
 
-                  };
+                category: validateCategory(req.body.category),
+
+                userId: req.body.userId
+
+            };
+
+            var errorFlag = business.businessName.message || business.businessDescription.message || business.businessAddress.message || business.location.message || business.category.message;
+
+            if (errorFlag) {
+
+                return res.status(400).json({ message: 'An error just occurred!', business: business });
             }
-      }]);
 
-      return UserValidation;
+            req.body = business;
+
+            return next();
+        }
+
+        /**
+         * @static
+         *
+         *@description -This method validates business Categories in WEConnect
+          *
+          * @param {object} req - The request payload sent to the router
+          * @param {object} res - The response payload sent back from the controller
+          * @param {object} next
+          * @returns {object} - status Message and logins user into WEConnect
+          *
+          * @memberOf UserController
+          *
+          */
+
+    }, {
+        key: 'validateBusinessUpdate',
+        value: function validateBusinessUpdate(req, res, next) {
+            var _req$body = req.body,
+                businessName = _req$body.businessName,
+                businessAddress = _req$body.businessAddress,
+                businessDescription = _req$body.businessDescription,
+                location = _req$body.location,
+                category = _req$body.category;
+
+
+            var businessUpdate = {};
+
+            if (businessName) {
+
+                businessUpdate.businessName = validateBusinessTextFields(req.body.businessName);
+            }
+
+            if (businessAddress) {
+
+                businessUpdate.businessAddress = validateBusinessTextFields(req.body.businessAddress);
+            }
+
+            if (businessDescription) {
+
+                businessUpdate.businessDescription = validateBusinessTextFields(req.body.businessDescription);
+            }
+
+            if (location) {
+
+                businessUpdate.location = validateBusinessTextFields(req.body.location);
+            }
+
+            if (category) {
+
+                businessUpdate.category = validateBusinessTextFields(req.body.category);
+            }
+
+            var errorFlag = void 0;
+
+            if (category) {
+
+                if (businessUpdate.category.message) {
+
+                    errorFlag = 1;
+                }
+            } else if (location) {
+
+                if (businessUpdate.location.message) {
+
+                    errorFlag = 1;
+                }
+            } else if (businessDescription) {
+
+                if (businessUpdate.businessDescription.message) {
+
+                    errorFlag = 1;
+                }
+            } else if (businessAddress) {
+
+                if (businessUpdate.businessAddress.message) {
+
+                    errorFlag = 1;
+                }
+            } else if (businessName) {
+
+                if (businessUpdate.businessName.message) {
+
+                    errorFlag = 1;
+                }
+            }
+
+            if (errorFlag) {
+
+                return res.status(400).json({ message: 'An error just occurred!', businessUpdate: businessUpdate });
+            }
+
+            req.body = businessUpdate;
+
+            return next();
+        }
+    }]);
+
+    return BusinessValidation;
 }();
+
+exports.default = BusinessValidation;
--- server/src/middlewares/BusinessValidation.js
+++ server/src/middlewares/BusinessValidation.js
@@ -1,10 +1,6 @@
 import InputFieldsValidation from '../helper/InputFieldsValidation';
 
-const {
-
-  validateLocation, validateCategory
-
-} = InputFieldsValidation;
+const { validateLocation, validateCategory, validateBusinessTextFields } = InputFieldsValidation;
 
 /**
  * @class InputFieldsValidaton
@@ -12,7 +8,7 @@ const {
  * @description Validation operations on Input fields
  *
  */
-class UserValidation {
+class BusinessValidation {
 
 /**
    * @static
@@ -31,20 +27,156 @@ class UserValidation {
 
     const business = {
 
-      businessName: req.body.businessName,
+      businessName: validateBusinessTextFields(req.body.businessName),
 
-      businessAddress: req.body.businessAddress,
+      businessAddress: validateBusinessTextFields(req.body.businessAddress),
 
-      businessDescription: req.body.businessDescription,
+      businessDescription: validateBusinessTextFields(req.body.businessDescription),
+
+      businessImage: req.body.businessImage,
 
       location: validateLocation(req.body.location),
 
       category: validateCategory(req.body.category),
 
+      userId: req.body.userId
+
     };
 
+    const errorFlag = business.businessName.message || business.businessDescription.message
+
+    || business.businessAddress.message || business.location.message || business.category.message;
+
+    if (errorFlag) {
+
+      return res.status(400).json({ message: 'An error just occurred!', business });
+
+    }
+
+    req.body = business;
+
+    return next();
 
   }
 
 
+  /**
+   * @static
+   *
+   *@description -This method validates business Categories in WEConnect
+    *
+    * @param {object} req - The request payload sent to the router
+    * @param {object} res - The response payload sent back from the controller
+    * @param {object} next
+    * @returns {object} - status Message and logins user into WEConnect
+    *
+    * @memberOf UserController
+    *
+    */
+  static validateBusinessUpdate(req, res, next) {
+
+    const {
+
+      businessName,
+
+      businessAddress,
+
+      businessDescription,
+
+      location,
+
+      category
+
+    } = req.body;
+
+    const businessUpdate = {};
+
+    if (businessName) {
+
+      businessUpdate.businessName = validateBusinessTextFields(req.body.businessName);
+
+    }
+
+    if (businessAddress) {
+
+      businessUpdate.businessAddress = validateBusinessTextFields(req.body.businessAddress);
+
+    }
+
+    if (businessDescription) {
+
+      businessUpdate.businessDescription = validateBusinessTextFields(req.body.businessDescription);
+
+    }
+
+    if (location) {
+
+      businessUpdate.location = validateBusinessTextFields(req.body.location);
+
+    }
+
+    if (category) {
+
+      businessUpdate.category = validateBusinessTextFields(req.body.category);
+
+    }
+
+    let errorFlag;
+
+    if (category) {
+
+      if (businessUpdate.category.message) {
+
+        errorFlag = 1;
+
+      }
+
+    } else if (location) {
+
+      if (businessUpdate.location.message) {
+
+        errorFlag = 1;
+
+      }
+
+    } else if (businessDescription) {
+
+      if (businessUpdate.businessDescription.message) {
+
+        errorFlag = 1;
+
+      }
+    } else if (businessAddress) {
+
+      if (businessUpdate.businessAddress.message) {
+
+        errorFlag = 1;
+
+      }
+
+    } else if (businessName) {
+
+      if (businessUpdate.businessName.message) {
+
+        errorFlag = 1;
+
+      }
+
+    }
+
+    if (errorFlag) {
+
+      return res.status(400).json({ message: 'An error just occurred!', businessUpdate });
+
+    }
+
+    req.body = businessUpdate;
+
+    return next();
+
+  }
+
 }
+
+
+export default BusinessValidation;
